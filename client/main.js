7059f036bc36395fc1d2c846d1db44235c4cab7e
Juan Sebastian Barragan Jeronimo <js.barragan796@uniandes.edu.co>
2018-05-20 11:51:20 -0500

    Accesibilidad: agregar el lang atribute a html

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+/* global $ */
 import React from "react";
 import {Meteor} from "meteor/meteor";
 import {render} from "react-dom";
@@ -5,6 +6,7 @@ import {render} from "react-dom";
 import {renderRoutes} from "../imports/Ui/Main/Routes.jsx";
 
 Meteor.startup(() => {
+  $("html").attr("lang", "es");
   render(renderRoutes(), 
   document.getElementById("render-target"));
 });
